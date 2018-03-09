bf5003f0df5fad26ced73be0a1b9c1db764ae1bc
amaurel <alexandre.maurel@gmail.com>
2018-03-09 16:39:33 +0100

    per data point

--- main.R
+++ main.R
@@ -1,9 +1,9 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx())  %>% 
-  select(.y, .ci, .ri) %>% 
-  group_by(.ci, .ri) %>%
-  summarise(log = log(mean(.y), base=as.double(ctx$op.value('base')))) %>%
+(ctx = tercenCtx()) %>% 
+  select(.y) %>% 
+  transmute(log = log(mean(.y), base=as.double(ctx$op.value('base')))) %>%
   ctx$addNamespace() %>%
   ctx$save()
+ 
