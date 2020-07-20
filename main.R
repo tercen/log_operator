f1666d1fcff70a74cf8e7facda7a3d3522810b0e
Alexandre Maurel <alexandre.maurel@gmail.com>
2020-07-20 16:28:57 +0000

    compute log per cell

--- main.R
+++ main.R
@@ -1,9 +1,10 @@
 library(tercen)
 library(dplyr)
 
-(ctx = tercenCtx()) %>% 
-  select(.y) %>% 
-  transmute(log = log(.y, base=as.double(ctx$op.value('base')))) %>%
+(ctx = tercenCtx())  %>% 
+  select(.y, .ci, .ri) %>% 
+  group_by(.ci, .ri) %>%
+  summarise(mean = log(mean(.y), base=as.double(ctx$op.value('base')))) %>%
   ctx$addNamespace() %>%
   ctx$save()
-  
+ 
