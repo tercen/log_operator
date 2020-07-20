b5971dc0e6be7b222c9b9bc15fb16e7d4e0c1158
Alexandre Maurel <alexandre.maurel@gmail.com>
2020-07-20 16:32:57 +0000

    log

--- main.R
+++ main.R
@@ -4,7 +4,7 @@ library(dplyr)
 (ctx = tercenCtx())  %>% 
   select(.y, .ci, .ri) %>% 
   group_by(.ci, .ri) %>%
-  summarise(mean = log(mean(.y), base=as.double(ctx$op.value('base')))) %>%
+  summarise(log = log(mean(.y), base=as.double(ctx$op.value('base')))) %>%
   ctx$addNamespace() %>%
   ctx$save()
  
